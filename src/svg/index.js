b773fc101265fb578e73574c5e47b8bed3919fbd
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-19 01:31:59 +0400

    fix absolute import error

---
+++ src/svg/index.js
@@ -0,0 +1,10 @@
+export { default as Hamburger } from "./Hamburger";
+export { default as Close } from "./Close";
+export { default as ArrowUp } from "./ArrowUp";
+export { default as CommentsIcon } from "./CommentsIcon";
+export { default as Suggestion } from "./Suggestion";
+export { default as ArrowLeft } from "./ArrowLeft";
+export { default as Plus } from "./Plus";
+export { default as NewIcon } from "./NewIcon";
+export { default as Edit } from "./Edit";
+export { default as Sherlock } from "./Sherlock";
