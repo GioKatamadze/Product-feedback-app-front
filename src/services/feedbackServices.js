f5be2960304a78f11a42157af693910001cb6525
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-18 15:55:46 +0400

    create feedback manipulation services to transfer data between data and front

---
+++ src/services/feedbackServices.js
@@ -0,0 +1,29 @@
+import instance from "./axios";
+
+export const getFeedbacks = () => {
+  return instance.get("/feedbacks");
+};
+
+export const addFeedback = (data) => {
+  return instance.post("/feedbacks/new", data);
+};
+
+export const getSingleFeedback = (id) => {
+  return instance.get("/feedbacks/" + id);
+};
+
+export const deleteFeedback = (id) => {
+  return instance.delete("/feedbacks/" + id);
+};
+
+export const editFeedback = (data) => {
+  return instance.put("/feedback", data);
+};
+
+export const addComment = (data) => {
+  return axios.post("/comment", data);
+};
+
+export const addReply = (data) => {
+  return axios.post("/reply", data);
+};
