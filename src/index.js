7cf20319211ba7e35da14cb3edf72568c68ca272
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-19 01:29:30 +0400

    create app layout using react router and pages components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import store from "store";
+const container = document.getElementById("root");
+const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <BrowserRouter>
