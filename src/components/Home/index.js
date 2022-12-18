52512c14499e0881f9a0335d11653f4b4299590c
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-18 23:28:56 +0400

    create absolute import file

---
+++ src/components/Home/index.js
@@ -0,0 +1,8 @@
+export { default as Header } from "./Header";
+export { default as Menu } from "./Menu";
+export { default as FilterHeader } from "./FilterHeader";
+export { default as FeedbackComponent } from "./FeedbackComponent";
+export { default as RoadmapPanel } from "./RoadmapPanel";
+export { default as ButtonsControl } from "./ButtonsControl";
+export { default as Suggestions } from "./Suggestions";
+export { default as Empty } from "./Empty";
