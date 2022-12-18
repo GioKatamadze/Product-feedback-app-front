0c958cd80c49610d41bf650db76b0e593f92d04f
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-18 17:35:18 +0400

    configure redux store

---
+++ src/store/redux.js
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  categoryReducer,
+  feedbacksReducer,
+  singleFeedbackReducer,
+  statusesReducer,
+  usersReducer,
+} from "./slices";
+
+const store = configureStore({
+  reducer: {
+    feedbacks: feedbacksReducer,
+    statuses: statusesReducer,
+    category: categoryReducer,
+    users: usersReducer,
+    singleFeedback: singleFeedbackReducer,
+  },
+});
+
+export default store;
