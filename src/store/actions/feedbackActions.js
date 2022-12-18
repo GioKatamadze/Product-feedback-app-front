2bb6ceebd5d07d3d369bc7d8c0c77ad45deb677f
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-18 17:31:59 +0400

    create feedback redux  action

---
+++ src/store/actions/feedbackActions.js
@@ -0,0 +1,31 @@
+import { getFeedbacks, getSingleFeedback } from "services";
+import { feedbacksActions } from "store";
+import { singleFeedbackActions } from "store/slices";
+
+export const fetchFeedbacks = () => {
+  return async (dispatch) => {
+    const fetchFeedbacksInfo = async () => {
+      const response = await getFeedbacks();
+      return response.data;
+    };
+
+    try {
+      const feedbacks = await fetchFeedbacksInfo();
+      dispatch(feedbacksActions.fetchFeedback(feedbacks));
+    } catch (error) {}
+  };
+};
+
+export const fetchSingleFeedback = (id) => {
+  return async (dispatch) => {
+    const fetchSingleFeedback = async () => {
+      const response = await getSingleFeedback(id);
+      return response.data;
+    };
+
+    try {
+      const feedback = await fetchSingleFeedback();
+      dispatch(singleFeedbackActions.fetchFeedback(feedback));
+    } catch (error) {}
+  };
+};
