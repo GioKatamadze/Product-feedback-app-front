609bacd656fd0de1b7a3df5500182d8c2c92fb53
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-18 15:23:18 +0400

    create necessary svg files with absolute import feature

---
+++ src/svg/Hamburger.jsx
@@ -0,0 +1,16 @@
+const Hamburger = (props) => {
+    return (
+      <svg
+        onClick={props.onClick}
+        width='20'
+        height='17'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <g fill='#FFF' fillRule='evenodd'>
+          <path d='M0 0h20v3H0zM0 7h20v3H0zM0 14h20v3H0z' />
+        </g>
+      </svg>
+    );
+  };
+  
+  export default Hamburger;
