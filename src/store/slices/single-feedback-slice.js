a70be4c9bb3c77d50198919b2706bbdfa13f8f39
Gio Katamadze <giokat.inbox@gmail.com>
2022-12-18 16:38:22 +0400

    create single feedback redux slice

---
+++ src/store/slices/single-feedback-slice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  feedback: {
+    category_id: 0,
+    commentAmount: 0,
+    comments: [],
+    description: "",
+    id: 0,
+    status_id: 0,
+    title: "",
+  },
+};
+
+const singleFeedbackSlice = createSlice({
+  name: "single feedback",
+  initialState,
+  reducers: {
+    fetchFeedbacks: (state, action) => {
+      state.feedback = action.payload;
+    },
+
+    addComment: (state, action) => {
+      const clone = state.feedback;
+      clone.comments.push(action.payload);
+      clone.commentAmount = clone.commentAmount + 1;
+      state.feedback = clone;
+    },
+
+    addReply: (state, action) => {
+      const clone = state.feedback;
+      const commentIndex = clone.comments.findIndex(
+        (comment) => comment.id === action.payload.commentId
+      );
+      clone.comments[commentIndex].replies.push(action.payload);
+      clone.commentAmount = clone.commentAmount + 1;
+      state.feedback = clone;
+    },
+  },
+});
+
+export const singleFeedbackReducer = singleFeedbackSlice.reducer;
+export const singleFeedbackActions = singleFeedbackSlice.actions;
